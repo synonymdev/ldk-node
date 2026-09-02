055cae6b1874f1081d0739f68d2818f67ccf9b33
Ovi Trif <ovitrif@proton.me>
2026-09-02 11:16:59 +0200

    chore: refresh exact-head Swift artifact

--- Package.swift
+++ Package.swift
@@ -13,11 +13,15 @@ let checksum = "3d4ed7123281235353cc7558317c3c15f94e0ff9adcab5fbb1fe35d572662f3d
 =======
 let tag = "v0.7.0-rc.67"
 <<<<<<< HEAD
+<<<<<<< HEAD
 let checksum = "2bb344a67eccd18708677190104e7769b26e603695f61318e47a794010f29905"
 >>>>>>> 049b116 (fix: classify broadcast backend results)
 =======
 let checksum = "ddbf716ae6d1bdaad8b10fa78abab7af4011956e4eb4d4b002f229dcb94f6c8c"
 >>>>>>> d7f3bfc (chore: refresh broadcast result artifacts)
+=======
+let checksum = "61565a56ecf33a1e8634de5e3be7d388f0579565161b1237c56756cc8b6638ce"
+>>>>>>> e359f19 (chore: refresh exact-head Swift artifact)
 let url = "https://github.com/synonymdev/ldk-node/releases/download/\(tag)/LDKNodeFFI.xcframework.zip"
 
 let package = Package(
